a627a52b4b7a3fa235e4527d3c0683dbfb530953
DeringSalazar <desalazarsa@est.utn.ac.cr>
2024-08-19 15:51:09 -0600

    FrmGenerador
    Se crea la lista de los tickets junto al frmgenerador y a este se le asigna una ventana llamada frmgenerar

---
+++ Tickets/ListaTickets.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tickets;
+
+import TipoEnum.Tipo;
+import java.util.ArrayList;
+
+public class ListaTickets {
+    private ArrayList<Ticket> listaTickets;
+
+    public ListaTickets() {
+        this.listaTickets = new ArrayList<>();
+    }
+    
+    public void agregarTicket(Ticket tickets){
+        this.listaTickets.add(tickets);
+    }
+    
+    public void eliminarPocision(int pos){
+        this.listaTickets.remove(pos);
+    }
+    
+    public void eliminarValor(int valor){
+        this.listaTickets.remove(valor);
+    }
+    
+    public Ticket buscarTicket(Ticket Tickets){
+         for (int i = 0; i < listaTickets.size(); i++){
+            if (this.listaTickets.get(i).getTipo()== null) {
+                Tickets = i;
+            }
+         }
+        return null;  
+    }
+}
